d1e47abb04382a75a849caffff7d642ecb9b0653
Spencer Davis <spencerdavis2000@yahoo.com>
2013-02-11 16:21:18 -0600

    Add pry to Gemfile for debugging. Add tests for board

--- game.rb
+++ game.rb
@@ -1,15 +1,22 @@
+require 'pry'
 class Game
   attr_reader :current_player
+  attr_accessor :board
 
   def initialize
     @current_player  = 'O'
+    @board = ['']*9
   end
 
   def play_move(move)
-    if @current_player == 'O'
+    if move == 1
       @current_player = 'X'
-    else
+    elsif move == 2
       @current_player = 'O'
     end
   end
+  def change_board(player, position)
+    @board[position] = player
+  end
 end
+#binding.pry
--- game_spec.rb
+++ game_spec.rb
@@ -6,10 +6,32 @@ describe Game do
     game.current_player.should == 'O' 
   end
 
-  it "changes current player after play a move" do
+  it "changes current player after play_move" do
     game = Game.new
+    game.play_move(2)
     game.current_player.should == 'O'
-    game.play_move(9)
+    game.play_move(1)
     game.current_player.should == 'X'
   end
+
+  it "player 1 adds a value to the board" do
+    game = Game.new
+    player_one = game.play_move(1)
+    game.change_board(player_one, 3)
+    game.board.should == ['', '', '', 'X', '', '', '', '', '']
+  end
+  it "player 2 adds a value to the board" do
+    game = Game.new
+    player_two = game.play_move(2)
+    game.change_board(player_two, 2)
+    game.board.should == ['', '', 'O', '', '', '', '', '', '']
+  end
+  
 end
+
+# while game.is_not_over?
+  # IO - ask user for move
+  # GAME LOGIC - game.play_move(move)
+  # AI - move = AI.generate_move(game.current_player, game.current_board)
+  # IO - menu, ask user for move, display board, etc.
+# end
